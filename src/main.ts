ad9dde46ac2bf4cea237acec02302d11dd369ec6
Ricky <thechaosmyth7@gmail.com>
2025-12-07 22:08:53 +0700

    add init plugin vee validate

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from "./App.vue"
 import "./assets/main.css"
 import { i18n } from "./plugins/language/index"
 import { installVueQuery } from "./plugins/tanstack"
+import { setupVeeValidate } from "./plugins/vee-validate"
 import router from "./router"
 
 const app = createApp(App)
@@ -12,6 +13,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+
 installVueQuery(app)
+setupVeeValidate(app)
 
 app.mount("#app")
---
+++ src/plugins/vee-validate/index.ts
@@ -0,0 +1,38 @@
+// src/plugins/vee-validate.ts
+import {
+  ErrorMessage as VErrorMessage,
+  Field as VField,
+  Form as VForm,
+  configure,
+} from "vee-validate"
+import type { App, Plugin } from "vue"
+
+import { i18n } from "@/plugins/language"
+
+function configureVeeValidate() {
+  configure({
+    validateOnBlur: true,
+    validateOnChange: true,
+    validateOnInput: false,
+    validateOnModelUpdate: true,
+    bails: true,
+    generateMessage: (context) => {
+      const t = i18n.global.t
+      const field = context.field || "field"
+      return t("validation.default", { field })
+    },
+  })
+}
+
+export const veeValidatePlugin: Plugin = {
+  install(app: App) {
+    app.component("VForm", VForm)
+    app.component("VField", VField)
+    app.component("VErrorMessage", VErrorMessage)
+    configureVeeValidate()
+  },
+}
+
+export function setupVeeValidate(app: App) {
+  app.use(veeValidatePlugin)
+}
