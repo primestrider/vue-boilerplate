f3feb517a0759222fab6e75079e29d9c8f888c8f
Ricky Ng <ricky@aptika.co.id>
2025-12-08 18:01:53 +0700

    feat: add example component

--- src/features/example/views/ExampleView.vue
+++ src/features/example/views/ExampleView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import ExampleComponent from "../components/ExampleComponent.vue"
+import ExampleForm from "../components/ExampleForm.vue"
 </script>
 <template>
   <div
     class="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-gray-100 px-6"
   >
     <ExampleComponent></ExampleComponent>
+    <ExampleForm></ExampleForm>
   </div>
 </template>
